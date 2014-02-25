3531d7646e45c4ac0781e5a2abc8bc609011ed93
Chris Knight <chris.knight@teligent.co.uk>
2014-02-25 09:01:19 +0000

    Fixed to support src directory

---
+++ lib/src/dircmd.c
@@ -0,0 +1,363 @@
+/******************************************************************************************************
+ *                                                                                                    *
+ *  D I R C M D . C                                                                                   *
+ *  ===============                                                                                   *
+ *                                                                                                    *
+ *  This is free software; you can redistribute it and/or modify it under the terms of the GNU        *
+ *  General Public License version 2 as published by the Free Software Foundation.  Note that I am    *
+ *  not granting permission to redistribute or modify this under the terms of any later version of    *
+ *  the General Public License.                                                                       *
+ *                                                                                                    *
+ *  This is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even    *
+ *  the impliedwarranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU          *
+ *  General Public License for more details.                                                          *
+ *                                                                                                    *
+ *  You should have received a copy of the GNU General Public License along with this program (in     *
+ *  the file "COPYING"); if not, write to the Free Software Foundation, Inc., 59 Temple Place -       *
+ *  Suite 330, Boston, MA 02111, USA.                                                                 *
+ *                                                                                                    *
+ ******************************************************************************************************/
+/**
+ *  @file
+ *  @brief Functions to process directories.
+ *  @version $Id: dircmd.c 1076 2010-10-02 20:38:45Z chris $
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <ctype.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <dircmd.h>
+
+#include "config.h"
+
+/******************************************************************************************************
+ * Prototypes															                              *
+ ******************************************************************************************************/
+static int listCompare (const void **item1, const void **item2);
+static int getEntryType (struct stat *fileStat);
+static char directoryVersionText[] = VERSION;
+
+/******************************************************************************************************
+ *                                                                                                    *
+ *  D I R E C T O R Y  V E R S I O N                                                                  *
+ *  ================================                                                                  *
+ *                                                                                                    *
+ ******************************************************************************************************/
+/**
+ *  @brief Return the version number of the dir library.
+ *  @result Pointer to the version number.
+ */
+char *directoryVersion(void)
+{
+	return directoryVersionText;
+}
+
+/******************************************************************************************************
+ *                                                                                                    *
+ *  S T R C A T _ C H                                                                                 *
+ *  =================                                                                                 *
+ *                                                                                                    *
+ ******************************************************************************************************/
+/**
+ *  @brief Internal fuction to add a char to the end of a string.
+ *  @param buff String to add the char to.
+ *  @param ch Char to add the the string.
+ *  @result Pointer to the string.
+ */
+static char *strcat_ch (char *buff, char ch)
+{
+	int l = strlen(buff);
+
+	if (l)
+	{
+		if (buff[l - 1] != ch)
+		{
+			buff[l + 1] = 0;
+			buff[l] = ch;
+		}
+	}
+	else
+	{
+		buff[0] = ch;
+		buff[1] = 0;
+	}
+
+	return buff;
+}
+
+/******************************************************************************************************
+ *                                                                                                    *
+ *  D I R E C T O R Y  L O A D  I N T                                                                 *
+ *  =================================                                                                 *
+ *                                                                                                    *
+ ******************************************************************************************************/
+/**
+ *  @brief Read the contents of a directory into memory.
+ *  @param inPath The path to the directory to be process.
+ *  @param partPath If it is recursive keep the subdirs to be added to t.
+ *  @param findFlags Various options to select what files to read.
+ *  @param Compare Function to compare two files.
+ *  @param fileList Where to save the directory.
+ *  @result The number of files found.
+ */
+static int directoryLoadInt (char *inPath, char *partPath, int findFlags,
+		int(*Compare)(DIR_ENTRY *f1, DIR_ENTRY *f2),
+		void **fileList)
+{
+	DIR *dirPtr;
+	int filesFound = 0;
+	struct dirent *dirList;
+	char fullPath[PATH_SIZE], filePattern[PATH_SIZE], *endPath;
+
+	strcpy (fullPath, inPath);
+	if ((endPath = strrchr (fullPath, DIRSEP)) != NULL)
+	{
+		if (*(++endPath))
+		{
+			strcpy (filePattern, endPath);
+			*endPath = 0;
+		}
+		else
+			strcpy (filePattern, "*");
+	}
+	else
+	{
+		strcpy (filePattern, fullPath);
+		if (getcwd (fullPath, PATH_SIZE - 2) == NULL)
+			strcpy (fullPath, ".");
+		strcat_ch (fullPath, DIRSEP);
+	}
+
+	if (!(*fileList))
+	{
+		if ((*fileList = queueCreate ()) == NULL)
+			return 0;
+	}
+
+    /*------------------------------------------------------------------------*
+     * Open the directory we plan to view                                     *
+     *------------------------------------------------------------------------*/
+	if ((dirPtr = opendir (fullPath)) != NULL)
+	{
+		endPath = &fullPath[strlen(fullPath)];
+
+		while ((dirList = readdir (dirPtr)) != NULL)
+		{
+		    /*----------------------------------------------------------------*
+			 * Recursive directories, avoid '.' and '..'                      *
+             *----------------------------------------------------------------*/
+			if (findFlags & RECUDIR && (dirList -> d_name[0] != '.' || findFlags & SHOWALL) &&
+					strcmp (dirList -> d_name, ".") && strcmp (dirList -> d_name, ".."))
+			{
+				struct stat tempStat;
+				char tempPath[PATH_SIZE];
+				char subPath[PATH_SIZE];
+
+				*endPath = 0;
+				strcpy (tempPath, fullPath);
+				strcat_ch (tempPath, DIRSEP);
+				strcat (tempPath, dirList -> d_name);
+
+				if (lstat (tempPath, &tempStat) == 0)
+				{
+					if (getEntryType (&tempStat) & ONLYDIRS)
+					{
+						strcpy (tempPath, fullPath);
+						strcat_ch (tempPath, DIRSEP);
+						strcat (tempPath, dirList -> d_name);
+						strcat_ch (tempPath, DIRSEP);
+						strcat (tempPath, filePattern);
+
+						strcpy (subPath, partPath);
+						strcat (subPath, dirList -> d_name);
+						strcat_ch (subPath, DIRSEP);
+
+						filesFound += directoryLoadInt (tempPath, subPath, findFlags,
+								Compare, fileList);
+					}
+				}
+			}
+
+            /*----------------------------------------------------------------*
+             * Does this file match our pattern, if yes then show it to the   *
+			 * user.                                                          *
+             *----------------------------------------------------------------*/
+			if (matchLogic (dirList -> d_name, filePattern, findFlags))
+			{
+				DIR_ENTRY *saveEntry = malloc (sizeof (DIR_ENTRY));
+
+				saveEntry -> fileName = malloc (strlen (dirList -> d_name) + 1);
+				saveEntry -> fullPath = malloc (strlen (fullPath) + 1);
+				saveEntry -> partPath = malloc (strlen (partPath) + 1);
+
+				*endPath = 0;
+				strcpy (saveEntry -> fileName, dirList -> d_name);
+				strcpy (saveEntry -> fullPath, fullPath);
+				strcpy (saveEntry -> partPath, partPath);
+				strcpy (endPath, dirList -> d_name);
+				saveEntry -> doneCRC = saveEntry -> CRC = saveEntry -> match = 0;
+				saveEntry -> Compare = Compare;
+
+                /*------------------------------------------------------------*
+				 * The 'STAT' function we get the full low down on file and   *
+				 * stores it in fileStat                                      *
+                 *------------------------------------------------------------*/
+				lstat (fullPath, &saveEntry -> fileStat);
+
+				if (getEntryType (&saveEntry -> fileStat) & findFlags)
+				{
+					if (strlen (saveEntry -> fileName) > queueGetFreeData (*fileList))
+						queueSetFreeData (*fileList, strlen (saveEntry -> fileName));
+
+					queuePut (*fileList, saveEntry);
+					filesFound ++;
+				}
+				else
+				{
+					free (saveEntry -> partPath);
+					free (saveEntry -> fullPath);
+					free (saveEntry -> fileName);
+					free (saveEntry);
+				}
+			}
+		}
+        /*--------------------------------------------------------------------*
+         * All done so close the directory and tell them how many files and   *
+		 * directories were found                                             *
+         *--------------------------------------------------------------------*/
+		closedir (dirPtr);
+	}
+	return filesFound;
+}
+
+/******************************************************************************************************
+ *                                                                                                    *
+ *  D I R E C T O R Y  L O A D                                                                        *
+ *  ==========================                                                                        *
+ *                                                                                                    *
+ ******************************************************************************************************/
+/**
+ *  @brief Read the contents of a directory into memory.
+ *  @param inPath The path to the directory to be process.
+ *  @param findFlags Various options to select what files to read.
+ *  @param Compare Function to compare two files.
+ *  @param fileList Where to save the directory.
+ *  @result The number of files found.
+ */
+int directoryLoad (char *inPath, int findFlags,
+		int(*Compare)(DIR_ENTRY *f1, DIR_ENTRY *f2),
+		void **fileList)
+{
+	return directoryLoadInt (inPath, "", findFlags, Compare, fileList);
+}
+
+/******************************************************************************************************
+ *                                                                                                    *
+ *  D I R E C T O R Y  S O R T                                                                        *
+ *  ==========================                                                                        *
+ *                                                                                                    *
+ ******************************************************************************************************/
+/**
+ *  @brief Sort the directory.
+ *  @param fileList List of files.
+ *  @result 1 if OK.
+ */
+int directorySort (void **fileList)
+{
+	queueSort (*fileList, (void *)listCompare);
+	return 1;
+}
+
+/******************************************************************************************************
+ *                                                                                                    *
+ *  D I R E C T O R Y  P R O C E S S                                                                  *
+ *  ================================                                                                  *
+ *                                                                                                    *
+ ******************************************************************************************************/
+/**
+ *  @brief Funtion to process all the files in the directory.
+ *  @param dirEntry xx.
+ *  @param fileList Saved directory, loaded with directoryLoad.
+ *  @result Number of files processed.
+ */
+int directoryProcess (int(*ProcFile)(DIR_ENTRY *dirEntry),	void **fileList)
+{
+	DIR_ENTRY *readEntry;
+	int filesProcessed = 0;
+
+	while ((readEntry = (DIR_ENTRY *)queueGet (*fileList)) != NULL)
+	{
+		if (ProcFile (readEntry))
+			filesProcessed ++;
+
+		free (readEntry -> fileName);
+		free (readEntry -> fullPath);
+		free (readEntry -> partPath);
+
+		free (readEntry);
+	}
+	queueDelete (*fileList);
+	*fileList = NULL;
+	return filesProcessed;
+}
+
+/******************************************************************************************************
+ *                                                                                                    *
+ *  L I S T  C O M P A R E                                                                            *
+ *  ======================                                                                            *
+ *                                                                                                    *
+ ******************************************************************************************************/
+/**
+ *  @brief Function called to compare two directory items.
+ *  @param item1 First item to compare.
+ *  @param item2 Second item to compare with.
+ *  @result 1, 0 and -1 depending on callback function.
+ */
+static int listCompare (const void **item1, const void **item2)
+{
+	DIR_ENTRY *dirOne = (DIR_ENTRY *)*item1;
+	DIR_ENTRY *dirTwo = (DIR_ENTRY *)*item2;
+
+	return (dirOne -> Compare (dirOne, dirTwo));
+}
+
+/******************************************************************************************************
+ *                                                                                                    *
+ *  G E T  E N T R Y  T Y P E                                                                         *
+ *  =========================                                                                         *
+ *                                                                                                    *
+ ******************************************************************************************************/
+/**
+ *  @brief Check the directory entry type, should we add it.
+ *  @param fileStat File stat of the found file.
+ *  @result The type of the file fond.
+ */
+static int getEntryType (struct stat *fileStat)
+{
+	if (S_ISLNK(fileStat -> st_mode))
+		return ONLYLINKS;
+	if (S_ISDIR(fileStat -> st_mode))
+		return ONLYDIRS;
+	if (!fileStat -> st_mode || S_ISREG (fileStat -> st_mode))
+	{
+		if (fileStat -> st_mode & (S_IXOTH | S_IXGRP | S_IXUSR))
+			return ONLYEXECS;
+
+		return ONLYNEXCS;
+	}
+	if (S_ISBLK(fileStat -> st_mode) || S_ISCHR(fileStat -> st_mode))
+	{
+		return ONLYDEVS;
+ 	}
+ 	if (S_ISSOCK(fileStat -> st_mode))
+ 		return ONLYSOCKS;
+ 	if (S_ISFIFO(fileStat -> st_mode))
+ 		return ONLYPIPES;
+
+	printf ("OTHER: %X\n", fileStat -> st_mode);
+	return 0;
+}
---
+++ lib/src/dircmd.h
@@ -0,0 +1,291 @@
+/******************************************************************************
+ *                                                                            *
+ *  D I R U T I L S . H                                                       *
+ *  ===================                                                       *
+ *                                                                            *
+ *  This is free software; you can redistribute it and/or modify it under     *
+ *  the terms of the GNU General Public License version 2 as published by     *
+ *  the Free Software Foundation.  Note that I am not granting permission     *
+ *  to redistribute or modify this under the terms of any later version       *
+ *  of the General Public License.                                            *
+ *                                                                            *
+ *  This is distributed in the hope that it will be useful, but WITHOUT       *
+ *  ANY WARRANTY; without even the impliedwarranty of MERCHANTABILITY or      *
+ *  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License     *
+ *  for more details.                                                         *
+ *                                                                            *
+ *  You should have received a copy of the GNU General Public License         *
+ *  along with this program (in the file "COPYING"); if not, write to         *
+ *  the Free Software Foundation, Inc., 59 Temple Place - Suite 330,          *
+ *  Boston, MA 02111, USA.                                                    *
+ *                                                                            *
+ ******************************************************************************/
+/**
+ *  @file
+ *  @brief Headers for libdircmd.
+ *  @version $Id: dirutils.h 0 2010-09-30 00:00:00Z owner $
+ */
+#ifndef INCLUDE_DIRCMD_H
+#define INCLUDE_DIRCMD_H
+ 
+#include <time.h>
+#include <dirent.h>
+#include <limits.h>
+#include <values.h>
+#include <sys/stat.h>
+
+#ifdef DOS
+	#define DIRSEP			'\\'
+	#define PATHSEP			';'
+#else
+	#define DIRSEP			'/'
+	#define PATHSEP			':'
+#endif
+
+#define PATH_SIZE			PATH_MAX
+
+/** 
+ *  @def ALLFILES
+ *  @brief Match all file types in the directory.
+ */
+#define ALLFILES			0x007F
+
+/** 
+ *  @def ONLYNEXCS
+ *  @brief Match only non-executable files in the directory.
+ */
+#define ONLYNEXCS			0x0001
+
+/** 
+ *  @def ONLYDIRS
+ *  @brief Match only sub-directories in the directory.
+ */
+#define ONLYDIRS			0x0002
+
+/** 
+ *  @def ONLYLINKS
+ *  @brief Match only links in the directory.
+ */
+#define ONLYLINKS			0x0004
+
+/** 
+ *  @def ONLYEXECS
+ *  @brief Match only executable files in the directory.
+ */
+#define ONLYEXECS			0x0008
+
+/** 
+ *  @def ONLYFILES
+ *  @brief Match only files in the directory.
+ */
+#define ONLYFILES			0x0009
+
+/** 
+ *  @def ONLYDEV
+ *  @brief Match only DEVICES in the directory.
+ */
+#define ONLYDEVS			0x0010
+
+/** 
+ *  @def ONLYDEV
+ *  @brief Match only DEVICES in the directory.
+ */
+#define ONLYSOCKS			0x0020
+
+/** 
+ *  @def ONLYDEV
+ *  @brief Match only DEVICES in the directory.
+ */
+#define ONLYPIPES			0x0040
+
+/** 
+ *  @def USECASE
+ *  @brief Do a case dependant compare.
+ */
+#define USECASE				0x0100
+
+/** 
+ *  @def SHOWALL
+ *  @brief Show all files even those starting with a dot.
+ */
+#define SHOWALL				0x0200
+
+/** 
+ *  @def RECUDIR
+ *  @brief Include subdirectories in the directory.
+ */
+#define RECUDIR				0x0400
+
+/** 
+ *  @def MAX_COLUMNS
+ *  @brief Maximum number of columns allowed.
+ */
+#define MAX_COLUMNS			30
+
+/** 
+ *  @def COL_ALIGN_RIGHT
+ *  @brief Flag set if the column should be right aligned.
+ *
+ *  Used in COLUMN_DESC::attrib.
+ */
+#define COL_ALIGN_RIGHT		1	/* default left */
+
+/** 
+ *  @def COL_CAN_DELETE
+ *  @brief Flag set if the column can be deleted on small terminals.
+ *
+ *  Used in COLUMN_DESC::attrib.
+ */
+#define COL_CAN_DELETE		2	/* can delete column to save size */
+
+/** 
+ *  @def DISPLAY_HEADINGS
+ *  @brief Flag set if the headings should be added to the table.
+ *
+ *  Used in  displayColumnInit options.
+ */
+#define DISPLAY_HEADINGS	1
+#define DISPLAY_COLOURS		2
+
+#define DISPLAY_FIRST		1
+#define DISPLAY_INFO		2
+
+/**
+ *  @struct columnDesc dircmd.h
+ *  @brief Used when calling displayColumnInit.
+ */
+struct columnDesc
+{
+	/** The maximum size of the column */
+	int maxWidth;
+	/** The minimum size of the column */
+	int minWidth;
+	/** The starting column width */
+	int startWidth;
+	/** The gap after of the column */
+	int gap;
+	/** The colour to use for the column */
+	int colour;
+	/** The attributes of the column */
+	int attrib;
+	/** A text heading for the column */
+	char *heading;
+	/** Order to be deleted */
+	int priority;
+};
+
+/**
+ *  @typedef columnDesc COLUMN_DESC
+ *  @brief Type definition of the column description.
+ */
+typedef struct columnDesc COLUMN_DESC;
+
+/**
+ *  @struct dirEntry dircmd.h
+ *  @brief Used to pass directory information to call back functions.
+ */
+struct dirEntry
+{
+	/** Name of the file */
+	char *fileName;
+	/** Path to the file */
+	char *fullPath;
+	/** Path as it goes into sub directories */
+	char *partPath;
+	/** Directory information */
+	struct stat fileStat;
+	/** Has the CRC been calculated for this file */
+	unsigned int doneCRC;
+	/** CRC value for this file */
+	unsigned int CRC;
+	/** Was a match found for this file */
+	unsigned int match;
+	/** Pointer to function used to compare the files */
+	int(*Compare)(struct dirEntry *f1, struct dirEntry *f2);
+};
+
+/**
+ *  @typedef dirEntry DIR_ENTRY
+ *  @brief Type definition of the directory entry.
+ */
+typedef struct dirEntry DIR_ENTRY;
+
+/*
+ *  dircmd.c
+ */
+char *directoryVersion(void);
+int directoryLoad (char *inPath, int findFlags,
+		int(*Compare)(DIR_ENTRY *f1, DIR_ENTRY *f2),
+		void **fileList);
+int directorySort (void **fileList);
+int directoryProcess (int(*ProcFile)(DIR_ENTRY *f1), void **fileList);
+
+/*
+ *  crc.c
+ */
+int CRCFile (char *filename);
+
+/*
+ *  display.c
+ */
+void displayLine (void);
+char *displayCommaNumber (long long number, char *outString);
+char *displayDateString (time_t showDate, char *outString);
+void displaySetDateFormat (char *format, int which);
+char *displayFileSize (long long size, char *outString);
+char *displayRightsString (int userRights, char *outString);
+char *displayOwnerString (int ownerID, char *outString);
+char *displayGroupString (int groupID, char *outString);
+char *displayContextString (char *fullpath, char *outString);
+void displayGetWindowSize (void);
+void displayForceSize (int cols, int rows);
+int displayGetWidth (void);
+int displayGetDepth (void);
+int displayColumnInit (int colCount, COLUMN_DESC *colDesc[], int options);
+int displayInColumn (int column, char *format, ...);
+int displayInColour (int column, int colour, char *format, ...);
+void displayMatchWidth (void);
+void displaySomeLines (int lines);
+void displayAllLines (void);
+void displayNewLine (char flags);
+void displayDrawLine (char flags);
+void displayHeading (char flags);
+void displayBlank (char flags);
+void displayTidy (void);
+
+/*
+ *  list.c
+ */
+void *queueCreate (void);
+void  queueDelete (void *queueHandle);
+void *queueGet (void *queueHandle);
+void  queuePut (void *queueHandle, void *putData);
+void  queuePutSort (void *queueHandle, void *putData, 
+	  		int(*Compare)(const void *item1, const void *item2));
+void  queuePush (void *queueHandle, void *putData);
+void *queueRead (void *queueHandle, int item);
+void queueSetFreeData (void *queueHandle, unsigned long setData);
+unsigned long queueGetFreeData (void *queueHandle);
+unsigned long queueGetItemCount (void *queueHandle);
+void queueSort (void *queueHandle, 
+			int(*Compare)(const void *item1, const void *item2));
+
+
+/*
+ *  match.c
+ */
+int matchLogic (char *str, char *ptn, int flags);
+int matchPattern (char *str, char *ptn, int flags);
+
+/*
+ * config.c
+ */
+int configLoad (const char *configFile);
+void configFree ();
+int configSetValue (char *configName, char *configValue);
+int configSetIntValue (char *configName, int configValue);
+int configGetValue (char *configName, char *value);
+int configGetIntValue (const char *configName, int *value);
+
+
+#endif
