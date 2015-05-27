a551929485c98dc14d6a4af1fa70ed5bf2a590f3
ukchkn <chris.knight@teligent.co.uk>
2015-05-27 16:24:10 +0100

    Added quiet mode option

--- utils/src/hexDump.c
+++ utils/src/hexDump.c
@@ -40,11 +40,12 @@ int showDir (DIR_ENTRY *file);
  * Globals                                                                    *
  * 00 00 00 00 00 00 00 00-08 13 50 00 00 00 00 00 : ..........P.....         *
  *----------------------------------------------------------------------------*/
-COLUMN_DESC colDumpDescs[3] =
+COLUMN_DESC colDumpDescs[4] =
 {
 	{	6,	6,	6,	3,	0x0E,	COL_ALIGN_RIGHT,	"Offset",	1	},	/* 0 */
 	{	47,	47,	47,	3,	0x0A,	0,					"Hex",		0	},	/* 1 */
 	{	16,	16,	16,	0,	0x0B,	0,					"Text",		2	},	/* 2 */
+	{	47,	47,	47,	3,	0x0A,	0,					"",			0	},	/* 1 */
 };
 
 COLUMN_DESC *ptrDumpColumn[3] =
@@ -54,8 +55,14 @@ COLUMN_DESC *ptrDumpColumn[3] =
 	&colDumpDescs[2]
 };
 
+COLUMN_DESC *ptrDumpColQuiet[1] =
+{
+	&colDumpDescs[3],
+};
+
 int filesFound = 0;
 int displayColour = 0;
+int displayQuiet = 0;
 
 /**********************************************************************************************************************
  *                                                                                                                    *
@@ -95,7 +102,7 @@ int main (int argc, char *argv[])
 	if (argc == 1)
 	{
 		version ();
-		printf ("Enter the command: %s <filename>\n", basename (argv[0]));
+		printf ("Enter the command: %s [-q] <filename>\n", basename (argv[0]));
 		exit (1);
 	}
 	
@@ -112,6 +119,10 @@ int main (int argc, char *argv[])
 			case 'C':
 				displayColour = DISPLAY_COLOURS;
 				break;
+				
+			case 'q':
+				displayQuiet ^= 1;
+				break;
 			}
 		}
 		else
@@ -127,14 +138,28 @@ int main (int argc, char *argv[])
 
 	if (found)
 	{
-		if (!displayColumnInit (3, ptrDumpColumn, displayColour))
+		if (!displayQuiet)
 		{
-			fprintf (stderr, "ERROR in: displayColumnInit\n");
-			return 1;
+			if (!displayColumnInit (3, ptrDumpColumn, displayColour))
+			{
+				fprintf (stderr, "ERROR in: displayColumnInit\n");
+				return 1;
+			}
+		}
+		else
+		{
+			if (!displayColumnInit (1, ptrDumpColQuiet, 0))
+			{
+				fprintf (stderr, "ERROR in: displayColumnInit\n");
+				return 1;
+			}
 		}
 		directoryProcess (showDir, &fileList);
-		displayInColumn (1, "%d %s shown\n", filesFound, 
-				filesFound == 1 ? "File" : "Files");
+		if (!displayQuiet)
+		{
+			displayInColumn (1, "%d %s shown\n", filesFound, 
+					filesFound == 1 ? "File" : "Files");
+		}
 		displayNewLine(DISPLAY_INFO);
 		displayAllLines ();		
 
@@ -171,9 +196,9 @@ int showDir (DIR_ENTRY *file)
 	
 	if ((readFile = fopen ((char *)inFile, "rb")) != NULL)
 	{
-		displayInColumn (1, "%s", file -> fileName);
-		displayDrawLine (0);
-		displayHeading (0);
+		if (!displayQuiet) displayInColumn (1, "%s", file -> fileName);
+		if (!displayQuiet) displayDrawLine (0);
+		if (!displayQuiet) displayHeading (0);
 		
 		while ((read = fread (inBuffer, 1, 1024, readFile)) != 0)
 		{
@@ -189,9 +214,10 @@ int showDir (DIR_ENTRY *file)
 
 				if (j == 16)
 				{
-					displayInColumn (0, "%06X", filePosn);
-					displayInColumn (1, "%s", &saveHex[1]);
-					displayInColumn (2, "%s", saveChar);
+					int c = 0;
+					if (!displayQuiet) displayInColumn (c++, "%06X", filePosn);
+					displayInColumn (c++, "%s", &saveHex[1]);
+					if (!displayQuiet) displayInColumn (c++, "%s", saveChar);
 					displayNewLine(0);
 
 					filePosn += 16;
@@ -207,12 +233,13 @@ int showDir (DIR_ENTRY *file)
 		}
 		if (j)
 		{
-			displayInColumn (0, "%06X", filePosn);
-			displayInColumn (1, "%s", &saveHex[1]);
-			displayInColumn (2, "%s", saveChar);
+			int c = 0;
+			if (!displayQuiet) displayInColumn (c++, "%06X", filePosn);
+			displayInColumn (c++, "%s", &saveHex[1]);
+			if (!displayQuiet) displayInColumn (c++, "%s", saveChar);
 			displayNewLine(0);
 		}
-		displayDrawLine (0);
+		if (!displayQuiet) displayDrawLine (0);
 		displayAllLines ();
 		
 		fclose (readFile);
