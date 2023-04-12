8cd3348af2d6baa5e27e89a45a2e6b70b268bb59
Chris Knight <chris@theknight.co.uk>
2023-04-12 10:00:06 +0100

    Added buildDate.h

---
+++ others/buildDate.h
@@ -0,0 +1,9 @@
+/*********************************
+ * File created by: setBuildDate *
+ *********************************/
+#ifndef BUILDDATE_H
+#define BUILDDATE_H
+
+static const char *buildDate = "2023/04/12";
+
+#endif
